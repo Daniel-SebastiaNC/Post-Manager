00a45d0901d9c580eaae9eda39ffd962cc16f20b
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-06-21 22:56:03 -0300

    first commit

---
+++ src/main/java/dev/danielsebastian/postmanager/PostManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.danielsebastian.postmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostManagerApplication.class, args);
+    }
+
+}
