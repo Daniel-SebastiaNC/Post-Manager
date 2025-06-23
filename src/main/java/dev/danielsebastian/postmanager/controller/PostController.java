54c7619d16210564350e7f0c1911a91275da58ac
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-06-23 13:59:06 -0300

    feat: create PostController with createPost(POST) and getAllPosts(GET) endpoints

---
+++ src/main/java/dev/danielsebastian/postmanager/controller/PostController.java
@@ -0,0 +1,36 @@
+package dev.danielsebastian.postmanager.controller;
+
+import dev.danielsebastian.postmanager.dto.post.PostRequest;
+import dev.danielsebastian.postmanager.dto.post.PostResponse;
+import dev.danielsebastian.postmanager.mapper.post.PostMapper;
+import dev.danielsebastian.postmanager.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/post")
+@RequiredArgsConstructor
+public class PostController {
+    private final PostService postService;
+    private final PostMapper postMapper;
+
+    @PostMapping("/create")
+    public ResponseEntity<PostResponse> createPost(@RequestBody PostRequest postRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                postMapper.toResponse(
+                    postService.createPost(postRequest)
+                )
+        );
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<PostResponse>> getAllPosts() {
+        return ResponseEntity.ok(
+                postService.getAllPosts().stream().map(postMapper::toResponse).toList()
+        );
+    }
+}
