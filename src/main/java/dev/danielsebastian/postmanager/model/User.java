a16603ec4b0861a36d4e576b7b426ffb90b6b572
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-06-29 20:14:21 -0300

    feat: update User Entity for implements UserDetails and create method findByUsername on UserRepositoty to use on AuthService

--- src/main/java/dev/danielsebastian/postmanager/model/User.java
+++ src/main/java/dev/danielsebastian/postmanager/model/User.java
@@ -4,8 +4,10 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -13,7 +15,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,4 +28,29 @@ public class User {
 
     @OneToMany(mappedBy = "user")
     private List<Post> posts;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/dev/danielsebastian/postmanager/repository/UserRepository.java
+++ src/main/java/dev/danielsebastian/postmanager/repository/UserRepository.java
@@ -2,6 +2,10 @@ package dev.danielsebastian.postmanager.repository;
 
 import dev.danielsebastian.postmanager.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<UserDetails> findByUsername(String username);
 }
