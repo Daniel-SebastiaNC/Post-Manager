94e372e72ad628b642cc5a5d18c6d9ddba52e306
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-06-29 20:17:46 -0300

    feat: add loginUser method on UserService

--- src/main/java/dev/danielsebastian/postmanager/service/UserService.java
+++ src/main/java/dev/danielsebastian/postmanager/service/UserService.java
@@ -1,11 +1,19 @@
 package dev.danielsebastian.postmanager.service;
 
+import dev.danielsebastian.postmanager.dto.user.LoginResponse;
+import dev.danielsebastian.postmanager.dto.user.UserLoginRequest;
 import dev.danielsebastian.postmanager.dto.user.UserRegisterRequest;
+import dev.danielsebastian.postmanager.exception.UsernameOrPasswordInvalidException;
 import dev.danielsebastian.postmanager.mapper.user.UserMapper;
 import dev.danielsebastian.postmanager.model.User;
 import dev.danielsebastian.postmanager.repository.UserRepository;
+import dev.danielsebastian.postmanager.security.TokenService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,7 +22,10 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
+
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
 
     private final UserMapper userMapper;
 
@@ -31,5 +42,19 @@ public class UserService {
 
     public Optional<User> getUserById(Long id) {
         return userRepository.findById(id);
+    }
+
+    public LoginResponse loginUser(UserLoginRequest loginRequest){
+        try {
+            UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password());
+            Authentication authentication = authenticationManager.authenticate(userAndPass);
+
+            User user = (User) authentication.getPrincipal();
+
+            return new LoginResponse(tokenService.generateToken(user));
+
+        } catch (BadCredentialsException e) {
+            throw  new UsernameOrPasswordInvalidException("Invalid email or password");
+        }
     }
 }
