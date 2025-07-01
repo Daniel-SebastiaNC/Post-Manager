bfc3e501ff145337d95722c0e1d5761306a91ec0
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-07-01 19:27:37 -0300

    feat: add verifyToken method on TokenService

--- src/main/java/dev/danielsebastian/postmanager/security/TokenService.java
+++ src/main/java/dev/danielsebastian/postmanager/security/TokenService.java
@@ -2,11 +2,15 @@ package dev.danielsebastian.postmanager.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import dev.danielsebastian.postmanager.dto.user.JWTUserData;
 import dev.danielsebastian.postmanager.model.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.util.Optional;
 
 @Component
 public class TokenService {
@@ -23,5 +27,24 @@ public class TokenService {
                 .withExpiresAt(Instant.now().plusSeconds(86400))
                 .withIssuedAt(Instant.now())
                 .sign(this.algorithm);
+    }
+
+    public Optional<JWTUserData> verifyToken(String token) {
+        try {
+            DecodedJWT jwt = JWT
+                    .require(this.algorithm)
+                    .build()
+                    .verify(token);
+
+            return Optional.of(
+                    new JWTUserData(
+                            jwt.getClaim("id").asLong(),
+                            jwt.getSubject()
+                    )
+            );
+
+        } catch (JWTVerificationException e) {
+            return Optional.empty();
+        }
     }
 }
